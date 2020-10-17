88ba6292a4d8982339e72cfa6d733172db9df64c
gaboq98 <tortuloca@gmail.com>
2020-10-16 20:58:53 -0600

    Adding proyect

---
+++ TowerOfBabelSolver/Controller/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TowerOfBabelSolver.Model;
+
+namespace TowerOfBabelSolver.Controller
+{
+    class GameController
+    {
+
+        public MainWindow StartWindow { get; set; }
+        public FileManager FileManager { get; set; }
+        public Logic GameLogic { get; set; }
+        public Solver GameSolver { get; set; }
+
+        public GameController(MainWindow startWindow)
+        {
+            StartWindow = startWindow;
+            FileManager = new FileManager();
+            GameLogic = new Logic();
+            GameSolver = new Solver();
+        }
+    }
+}
